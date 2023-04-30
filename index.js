88b184f74e040823e89a1466ffbd9784406333bd
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-04-30 18:11:53 +0600

    vercel deply

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
 
-const categories = require('./data/Catagories.json')
+const categories = require('./data/Catagories.json');
+const news = require('./data/News.json')
 
 app.use(cors());
 
@@ -13,6 +14,24 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedId = news.find(n => parseInt(n._id) === id)
+    res.send(selectedId)
+})
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews);
+    }
 })
 
 app.listen(port, () => {
